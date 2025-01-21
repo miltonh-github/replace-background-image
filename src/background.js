c291ea19aae991bec139dc8b12a1772bc3e33726
miltonh-github <119451755+miltonh-github@users.noreply.github.com>
2025-01-21 16:50:01 -0700

    Use "all" for contextmenus

--- src/background.js
+++ src/background.js
@@ -5,35 +5,39 @@ function createContextMenu() {
   chrome.contextMenus.create({
     id: "replaceImageLocal",
     title: "Replace with Local File",
-    contexts: ["page"],
+    contexts: ["all"],
   });
   
   chrome.contextMenus.create({
     id: "replaceImageUrl",
     title: "Replace with image URL",
-    contexts: ["page"],
+    contexts: ["all"],
   });
 
   chrome.contextMenus.create({
     id: "saveLoad",
     title: "Save/Load",
+    contexts: ["all"],
   });
 
   chrome.contextMenus.create({
     id: "replaceImageReset",
     title: "Reset all replaced images",
+    contexts: ["all"],
   });
 
   chrome.contextMenus.create({
     id: "saveLocalJson",
     title: "Save Local JSON",
     parentId: "saveLoad",
+    contexts: ["all"],
   });
 
   chrome.contextMenus.create({
     id: "loadLocalJson",
     title: "Load Local JSON",
     parentId: "saveLoad",
+    contexts: ["all"],
   });
 
 
